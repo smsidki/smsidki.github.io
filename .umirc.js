9fdad3d3bf1ed19c516dfe13efc5b6dfc4528a42
smsidki_ <sidqi_sm@nexsoft.co.id>
2019-03-04 21:36:15 +0700

    Init project

---
+++ .umirc.js
@@ -0,0 +1,29 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    'umi-plugin-gh-pages',
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'smsidki.github.io',
+      dll: true,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export default function() {
+  return (
+    <div>
+      <h2>It Works!</h2>
+    </div>
+  );
+}
