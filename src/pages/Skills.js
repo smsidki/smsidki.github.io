d2df71bf8dd455348a62842feb19d043896544a9
smsidki_ <sidqi_sm@nexsoft.co.id>
2019-03-08 18:27:35 +0700

    Prepare template for skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,6 +2,87 @@
  * title: smsidki | Skills
  */
 
-import OnProgress from '@/components/Maintenance/OnProgress';
+import React from 'react';
+import 'ant-design-pro/dist/ant-design-pro.css';
+import { Radar } from 'ant-design-pro/lib/Charts';
 
-export default () => <OnProgress/>;
+class Skills extends React.Component {
+
+  skills = [
+    {
+      name: ' ',
+      label: 'Apache Struts 2',
+      value: 'once upon a time'
+    },
+    {
+      name: ' ',
+      label: 'JSP',
+      value: 'once upon a time'
+    },
+    {
+      name: ' ',
+      label: 'ZKoss',
+      value: 'once upon a time'
+    },
+    {
+      name: ' ',
+      label: 'Camunda BPMN',
+      value: 'seldom'
+    },
+    {
+      name: ' ',
+      label: 'Elastic Search',
+      value: 'occasionally'
+    },
+    {
+      name: ' ',
+      label: 'Quartz Scheduler',
+      value: 'often'
+    },
+    {
+      name: ' ',
+      label: 'MySQL',
+      value: 'daily'
+    },
+    {
+      name: ' ',
+      label: 'Thymeleaf',
+      value: 'seldom'
+    },
+    {
+      name: ' ',
+      label: 'React',
+      value: 'often'
+    },
+    {
+      name: ' ',
+      label: 'Umi',
+      value: 'often'
+    },
+    {
+      name: ' ',
+      label: 'SQL Server',
+      value: 'seldom'
+    },
+    {
+      name: ' ',
+      label: 'Spring Boot',
+      value: 'daily'
+    }
+  ];
+
+  render() {
+    return (
+        <div>
+          <Radar
+            height={350}
+            hasLegend={false}
+            data={this.skills}
+          />
+        </div>
+    );
+  }
+
+}
+
+export default Skills;
